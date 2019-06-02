62cdc64784e71c9516e525c98c1b0ba5cb565378
Zeke Sikelianos <zeke@sikelianos.com>
2019-06-01 22:38:54 -0700

    give up on the mac app store

--- script/build.sh
+++ script/build.sh
@@ -16,32 +16,32 @@ electron-packager . \
   --platform=darwin 
 
 # sign macOS build
-DEBUG=electron-osx-sign* electron-osx-sign \
-  dist/Illuminati-darwin-x64/Illuminati.app
+DEBUG=electron-osx-sign* electron-osx-sign dist/Illuminati-darwin-x64/Illuminati.app
 
-# package for mas
-electron-packager . \
-  --app-bundle-id=com.sikelianos.zeke.illuminati \
-  --app-version="$npm_package_version" \
-  --arch=x64 \
-  --build-version="$npm_package_build" \
-  --extend-info=build/info.plist \
-  --icon=dist/icon.icns \
-  --out=dist \
-  --platform=mas
+# # package for mas
+# electron-packager . \
+#   --app-bundle-id=com.sikelianos.zeke.illuminati \
+#   --app-version="$npm_package_version" \
+#   --arch=x64 \
+#   --build-version="$npm_package_build" \
+#   --extend-info=build/info.plist \
+#   --icon=dist/icon.icns \
+#   --out=dist \
+#   --platform=mas
 
-# sign mas build
-DEBUG=electron-osx-sign* electron-osx-sign \
-  dist/Illuminati-mas-x64/Illuminati.app \
-  --provisioning-profile=build/embedded.provisionfile
+# # sign mas build
+# DEBUG=electron-osx-sign* electron-osx-sign \
+#   dist/Illuminati-mas-x64/Illuminati.app \
+#   --provisioning-profile=build/embedded.provisionfile
 
-# flatten mas build
-DEBUG=electron-osx-flat* electron-osx-flat dist/Illuminati-mas-x64/Illuminati.app \
-  --pkg dist/illuminati.pkg
+# # flatten mas build
+# DEBUG=electron-osx-flat* electron-osx-flat dist/Illuminati-mas-x64/Illuminati.app \
+#   --pkg dist/illuminati.pkg
 
 # create mac zip (for auto-updater)
-# electron-installer-zip dist/Illuminati-darwin-x64 dist/illuminati-mac.zip
+# See https://github.com/electron/update-electron-app#faq
+electron-installer-zip dist/Illuminati-darwin-x64 dist/illuminati-mac.zip
 
 # create mac dmg
-# create-dmg dist/Illuminati-darwin-x64/Illuminati.app
-# mv "Illuminati-$npm_package_version.dmg" dist/illuminati.dmg
+create-dmg dist/Illuminati-darwin-x64/Illuminati.app
+mv "Illuminati-$npm_package_version.dmg" dist/illuminati.dmg
